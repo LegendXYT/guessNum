6acbca181d23364315210bedf48daf20d83c7acc
LegendXYT <46520558+LegendXYT@users.noreply.github.com>
2019-03-03 07:52:43 +1100

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -5,24 +5,7 @@ const prefix = botconfig.prefix
 
 module.exports.run = async (bot, message, args) => {
 
-    const Discord = require("discord.js")
-const botconfig = require("../botconfig.json");
-const prefix = botconfig.prefix
-
-
-module.exports.run = async (bot, message, args) => {
-
     message.channel.send("```fix\nprefix - n/\nguess - create a guessing game\ninvite - invite me to your server\nserver - join our support server\nping - check the bots ping status")
-}
-
-
-module.exports.config = {
-    name: "help",
-    aliases: ["h", "halp", "commands", "info", "information"],
-    usage: "d!help",
-    description: "Shows the list of commands for this bot",
-    accessableby: "Members"
-  }
 }
 
 
